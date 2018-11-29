1e8d86913e297d0f9b1dd97ccad88244988e4eb3
everthx <fernando.luna16@tectijuana.edu.mx>
2018-11-28 22:34:07 -0800

    Created media file so serv Articulos Images

--- Articulos/models.py
+++ Articulos/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Articulo(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
-    image = models.ImageField( )
+    image = models.ImageField( upload_to="Articulos")
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
@@ -13,7 +13,7 @@ class Articulo(models.Model):
         ordering = ["-created"]
     
     def Article(self):
-        cadena = "{0} created: {1}"
+        cadena = "{0},           [ created: {1} ]"
         return cadena.format(self.title, self.created)
 
     def __str__(self):
--- webpersonal/urls.py
+++ webpersonal/urls.py
@@ -17,9 +17,16 @@ from django.contrib import admin
 from django.urls import path
 from core import views  #Importing the vews from our core APP
 
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name = "home"),
     path('directorio/', views.directorio, name="directorio"),
     path('bolsa/', views.bolsa, name="bolsa-de-trabajo"),
 ]
+
+
+if settings.DEBUG:  # Displays media content from Dev site
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL,  document_root= settings.MEDIA_ROOT)
