08ab417a2e343aab7244001cdd3e4000f78028a4
everthx <fernando.luna16@tectijuana.edu.mx>
2018-12-01 10:06:00 -0800

    PostgreSQL

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'webpersonal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'techschool',
+        'USER': 'admin',
+        'PASSWORD': 'psw123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
