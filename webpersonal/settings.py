a49289194ae165e4d9dff9898c5592fdedda8cce
everthx <fernando.luna16@tectijuana.edu.mx>
2018-12-01 10:28:24 -0800

    Requierments

--- webpersonal/settings.py
+++ webpersonal/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#*gode$f$)08dfczn*c!sd@*%8@^d7$=ov+*3r_*(ke&z$h@+&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
